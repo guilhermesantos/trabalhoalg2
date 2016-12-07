baeedf64c4e64b9b186b8649c74feab71caef29e
guilherme <guilherme.milan9@gmail.com>
2016-12-07 21:32:24 -0200

    Comentarios no header do grafo

--- grafo_lista.h
+++ grafo_lista.h
@@ -26,16 +26,33 @@ typedef struct vertice {
 typedef struct grafo {
     //Vetor de vertices
     vertice *vertices;
+    //quantidade de vertices no grafo
     int qtd_vertices;
 } grafo;
 
+//Funcao que aloca espaco pro grafo e retorna seu ponteiro
 grafo *cria_grafo();
+
+//Funcao que insere vertice no grafo
 grafo *insere_vertice(grafo *g, char *valor);
+
+//Imprime um grafo
 void imprime_grafo(grafo *g);
+
+//Gera o codigo dot, linguagem do graphviz, do grafo e o retorna
+//como uma string que pode ser gravada em arquivo ou exibida na tela
 char *gera_codigo_dot(grafo *g);
 
+//insere uma aresta entre dois vertices com base em seu indice no vetor de vertices.
+//Acabou por ser usada apenas internamente, o programa principal utiliza a funcao que insere aresta por valor
 grafo *insere_aresta_por_numero(grafo *g, int numero_vertice_origem, int numero_vertice_destino);
+
+//Faz a mesma coisa que o insere aresta por numero, mas faz a aresta bidirecional
 grafo *insere_aresta_bidirecional_por_numero(grafo *g, int numero_vertice_origem, int numero_vertice_destino);
+
+//Insere a aresta com base no valor dos vertices. Utiliza internamente a funcao insere aresta por numero
 grafo *insere_aresta_por_valor(grafo *g, char *valor_vertice_origem, char *valor_vertice_destino);
+
+//Faz a mesma coisa que o insere aresta por valor, mas faz a aresta bidrecional
 grafo *insere_aresta_bidirecional_por_valor(grafo *g, char *valor_vertice_origem, char *valor_vertice_destino);
 #endif // GRAFO
